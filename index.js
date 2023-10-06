b494eae3b3e66bf1feb870b8840bbacdd40374ed
gargchiraag <chiraag.garg@chordify.com>
2023-10-06 18:26:46 +0530

    task done using javascript

--- index.js
+++ index.js
@@ -5,7 +5,23 @@ let addBtn=document.getElementById("addBtn");
 let index=1;
 let insertData=()=>{
     let key="key"+(index++);
-    localStorage.setItem(key,JSON.stringify({"Name":name.value,"Email":email.value,"Phone Number":number.value}));
-    console.log(JSON.parse(localStorage.getItem("key1")))
+    localStorage.setItem(key,JSON.stringify({"Name":name.value,"Email":email.value,"phoneNumber":number.value}));
+    let data=(JSON.parse(localStorage.getItem("key1")))
+    let table=document.getElementById("tableData");
+    let row=table.insertRow(1);
+    let cell1=row.insertCell(0);
+    let cell2=row.insertCell(1);
+    let cell3=row.insertCell(2);
+    let cell4=row.insertCell(3);
+    cell1.innerHTML=data.Name;
+    cell2.innerHTML=data.Email;
+    cell3.innerHTML=data.phoneNumber;
+    let deleteButton = document.createElement("BUTTON");
+    deleteButton.innerHTML = "Delete";
+    cell4.appendChild(deleteButton);
+    deleteButton.addEventListener("click",()=>{
+        table.deleteRow(row.rowIndex);
+        localStorage.removeItem(key);
+    })
 }
 addBtn.addEventListener("click",insertData);
